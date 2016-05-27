8d221631f388e2dbbdaeedd7e70c70360df4992f
Renato Chaves <renato.g.chaves@gmail.com>
2016-05-27 19:58:07 -0300

    Condição para mercado fechado/aberto

--- cartolina.js
+++ cartolina.js
@@ -1,3 +1,7 @@
 $(function () {
-    $('.cartola-card__comparar-parciais>div:last-child').append('<div class="cartola__button cartola__button--verde" ng-click="ctrl.openComparar()"><span class="cartola__button__texto">ver todos</span></div>');
+        if ($('.cartola-countdown-fechamento').length > 0) {
+            //inserir link para comparação com amigos 
+        } else {
+            $('.cartola-card__comparar-parciais>div:last-child').append('<div class="cartola__button cartola__button--verde" ng-click="ctrl.openComparar()"><span class="cartola__button__texto">ver todos</span></div>');
+        }
 });

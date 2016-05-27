54ca6598b0a69e5013e0d73a7a3bf7f18970f64d
Renato Chaves <renato.g.chaves@gmail.com>
2016-05-27 20:58:51 -0300

    Busca times dos amigos

--- cartolina.js
+++ cartolina.js
@@ -1,6 +1,22 @@
 $(function () {
+    function buscarAmigos(token) {
+        $.ajax({
+            url: 'https://api.cartolafc.globo.com/auth/amigos',
+            headers: {
+                'X-GLB-Token': token
+            },
+            error: function (errorObj) {
+                console.log(errorObj);
+            },
+            success: function (data) {
+                console.log(data);
+            }
+        });
+    }
+
     var token = document.cookie.match('GLBID=([^;]+)')[1];
     if (token) {
+        buscarAmigos(token);
         if ($('.cartola-countdown-fechamento').length > 0) {
             //inserir link para comparação com amigos 
         } else {

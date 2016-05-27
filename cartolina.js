38736c512ecebbfc147d037433247bfd4b62b58c
Renato Chaves <renato.g.chaves@gmail.com>
2016-05-26 22:15:41 -0300

    Botão "Ver Todos" na section de comparar parciais
    Apenas insere na página o botão, que ainda não funciona

---
+++ cartolina.js
@@ -0,0 +1,3 @@
+$(function () {
+    $('.cartola-card__comparar-parciais>div:last-child').append('<div class="cartola__button cartola__button--verde" ng-click="ctrl.openComparar()"><span class="cartola__button__texto">ver todos</span></div>');
+});

3e8d6c429c9c4838cf2d1af82401c0fc88e03923
Renato Chaves <renato.g.chaves@gmail.com>
2016-05-27 19:58:40 -0300

    Pega token pelo cookie
    Será útil para obter os times dos amigos

--- cartolina.js
+++ cartolina.js
@@ -1,7 +1,10 @@
 $(function () {
+    var token = document.cookie.match('GLBID=([^;]+)')[1];
+    if (token) {
         if ($('.cartola-countdown-fechamento').length > 0) {
             //inserir link para comparação com amigos 
         } else {
             $('.cartola-card__comparar-parciais>div:last-child').append('<div class="cartola__button cartola__button--verde" ng-click="ctrl.openComparar()"><span class="cartola__button__texto">ver todos</span></div>');
         }
+    }
 });

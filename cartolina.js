1f33cd39dde3d9df7e2e95fbe31d13a51290dd93
Renato Chaves <renato.g.chaves@gmail.com>
2016-05-27 21:41:17 -0300

    Link para exibir liga fake dos Amigos do FB

--- cartolina.js
+++ cartolina.js
@@ -14,11 +14,49 @@ $(function () {
         });
     }
 
+    function buscarProprioTime(token, callback) {
+        $.ajax({
+            url: 'https://api.cartolafc.globo.com/auth/time',
+            headers: {
+                'X-GLB-Token': token
+            },
+            error: function (errorObj) {
+                console.log(errorObj);
+            },
+            success: function (data) {
+                callback(data);
+            }
+        });
+    }
+
     var token = document.cookie.match('GLBID=([^;]+)')[1];
     if (token) {
         buscarAmigos(token);
         if ($('.cartola-countdown-fechamento').length > 0) {
-            //inserir link para comparação com amigos 
+            buscarProprioTime(token, function (data) {
+                $('section.cartola-ligas header').after('\n\
+                    <div>\n\
+                        <div card-liga="" liga="liga" time-id-usuario="ctrl.timeService.dadosTime.time_id">\n\
+                            <a class="cartola-card cartola-card--lista" ui-sref="liga({ slug: ctrl.liga.slug })">\n\
+                                <div class="cartola-card-liga-thumb">\n\
+                                    <img class="cartola-card-liga-thumb__img" alt="Amigos do FB" title="Amigos do FB" src="' + data.time.foto_perfil + '">\n\
+                                </div>\n\
+                                <div class="row collapse">\n\
+                                    <div class="medium-15 column">\n\
+                                        <div class="cartola-card-liga-info">\n\
+                                            <div class="cartola-card-liga-info__nome">Amigos do FB</div>\n\
+                                        </div>\n\
+                                    </div>\n\
+                                    <div class="medium-9 column">\n\
+                                        <div class="cartola-card-liga-pontuacao">\n\
+                                            <span class="cartola-liga-ranking__posicao">?°</span>\n\
+                                        </div>\n\
+                                    </div>\n\
+                                </div>\n\
+                            </a>\n\
+                        </div>\n\
+                    </div>');
+            });
         } else {
             $('.cartola-card__comparar-parciais>div:last-child').append('<div class="cartola__button cartola__button--verde" ng-click="ctrl.openComparar()"><span class="cartola__button__texto">ver todos</span></div>');
         }
